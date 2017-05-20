a424651df17cdbdcaba48ae29f528ef87d8185df
yu.xiao <yu.xiao@happylifeplat.com>
2017-05-20 15:59:11 +0800

    docker file

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
-FROM registry.wang-guanjia.com:5000/java:8
+FROM ubuntu:16.04
 MAINTAINER happylifeplat
 VOLUME /tmp
 EXPOSE 8093
 ENV TZ=Asia/Shanghai
-#定义环境变量，脚本中需要获取
-ENV env=dev
+ENV PATH=$PATH:/java/jre/bin
 ENV APP_NAME=happylife-service-search
 ENV JAR_PATH=/happylife-service-search.jar
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
--- happylife-service-search/src/test/java/com/happylifeplat/service/search/client/ClientTest.java
+++ happylife-service-search/src/test/java/com/happylifeplat/service/search/client/ClientTest.java
@@ -1,5 +1,18 @@
 package com.happylifeplat.service.search.client;
 
+import com.happylifeplat.facade.search.service.ElasticSearchService;
+import com.happylifeplat.service.search.helper.LogUtil;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * <p>Description: .</p>
  * <p>Company: 深圳市旺生活互联网科技有限公司</p>
@@ -12,7 +25,26 @@ package com.happylifeplat.service.search.client;
  */
 public class ClientTest {
 
+    /**
+     * logger
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientTest.class);
+
     public static void main(String[] args) {
+        String clusterName = "elasticsearch53";
+        String ip = "120.76.52.162";
+        Integer port = 9300;
+        //es集群的设置信息
+        Settings settings = Settings.builder().put("client.transport.sniff", true)
+                .put("cluster.name", clusterName).build();
+        try {
+            TransportClient client = new PreBuiltTransportClient(settings)
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(ip), port));
+            final SearchResponse searchResponse = client.prepareSearch("goods").setTypes("goods").get();
+            LogUtil.info(LOGGER, "数据:{}", searchResponse);
+        } catch (UnknownHostException e) {
+            LogUtil.error(LOGGER, "elasticsearch UnknownHostException 客户端连接失败：{}", e::getMessage);
+        }
 
     }
 }
